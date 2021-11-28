ee42854c4dcc5ad2d49e302fcd03dff26d4c77a8
Poulomi Saha <85102202+poulomi-tech@users.noreply.github.com>
2021-11-28 19:12:53 +0530

    Add files via upload

---
+++ Demo3/src/com/gl/demo/Demo3.java
@@ -0,0 +1,20 @@
+package com.gl.demo;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float num1, num2, result1;
+num1 = 5;
+num2 = 8;
+
+result1 = num1 + num2 ;
+System.out.println(result1);
+
+int num3 = 9;
+String result2 = (num3 % 2 == 0) ? "even" : "odd" ;
+
+System.out.println(result2);
+	}
+
+}
